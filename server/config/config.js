6055ad438adf26b97bfe27424345cd44ebcc8930
dylanvansteen <dylanvansteen@gmail.com>
2017-01-05 19:06:40 +0100

    cahnge to db

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const envKeys = {
     NODE_ENV: process.env.NODE_ENV,
     PORT: process.env.PORT,
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGODB_URI
 }
 
 var env = envKeys.NODE_ENV || 'development';
