a0f4bdda8c1a263b07c7c4ea8c7af11e53bc6511
dylanvansteen <dylanvansteen@gmail.com>
2017-01-08 15:32:53 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
 
-if (environment.production) {
+if (process.env.NODE_ENV === 'production') {
   enableProdMode();
 }
 
