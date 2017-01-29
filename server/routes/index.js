273be98deb402fd75c00d2a10d2e379a294712bf
dylanvansteen <dylanvansteen@gmail.com>
2017-01-29 13:03:45 +0100

    remove

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/folder', require('./folder-route'));
-router.use('/authorization', require('./authorization-route'));
-
-module.exports = router;
